1e76d8083202a7f2331cfb21ede4b9505ffe121b
Forrest Reiland <you@example.com>
2020-12-31 13:21:55 -0800

    useffect hook with json object

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,1 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+export default function AppTwo () {
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  console.log('render')
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))}, [resourceType])
+  
+
+  return (
+    <React.Fragment>
+      <div className='test'>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+
+    </React.Fragment>
+  )
+
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppTwo from './AppTwo';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AppTwo />
   </React.StrictMode>,
   document.getElementById('root')
 );
